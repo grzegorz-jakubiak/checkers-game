ed906c0372fced72347ee0ee32dfa5b319ae414c
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2020-10-20 21:04:28 +0200

    Implement board
    Closes #1

---
+++ lib/checkers/board.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Checkers
+  class Board
+    def initialize
+      @board = set_board
+    end
+
+    private
+
+    def set_board
+      Matrix[
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [-1, 0, -1, 0, -1, 0, -1, 0],
+        [0, -1, 0, -1, 0, -1, 0, -1],
+        [-1, 0, -1, 0, -1, 0, -1, 0]
+      ]
+    end
+  end
+end
