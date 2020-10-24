72e8c56b4b78b8633fceb33feb54e154b0dd3da0
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2020-10-24 21:06:29 +0200

    Put consts in module

---
+++ lib/checkers/gui.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Checkers
+  module GUI
+    SQUARE_SIZE = 50
+    CIRCLE_TRANSLATION = SQUARE_SIZE * Integer.sqrt(2) / 2
+    RADIUS = 20
+    MOVE_RADIUS = 10
+  end
+end
